88b21d9035cc34165e0cde93eb2de37f142c96da
sabadia <919.hasan@gmail.com>
2020-07-25 15:23:15 +0600

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+$(document).ready(function () {
+    $("h1").css({
+        color: "black",
+        border: "2px solid green",
+    });
+});
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $("h1").css({
+    color: "black",
+    border: "2px solid green",
+  });
+});
